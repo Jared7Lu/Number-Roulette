8fbf17d4bd6fd0999fd6cca56e250ec8f05df735
jalp21@icloud.com <jalp21@icloud.com>
2022-10-26 20:17:41 -0700

    Layout of ideas is complete

---
+++ main.js
@@ -0,0 +1,23 @@
+// Main varaibles
+
+    // Create var- player choice, ai choice
+    // Create function to randomly appoint number to ai
+    // create function to register players choice
+
+// Player shooting
+
+    // create function to register players choice of attack 
+    // create function to grey out number once chosen if wrong
+    // Make color red if it is a hit
+
+// Ai shooting
+
+    // create function to make ai choose random number
+    // create function to grey out number once chosen if wrong
+    // Make color red if it is a hit
+
+// what happens when wrong number is chosen
+
+    // create function to decide if player chose the bullet
+    // If the bulllet was chosen say "You Die"
+    // If the bullter was not chosen say 'You Live'
