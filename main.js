ec7549d0fecd71ab16535d7fe2d9cbd05ca4a908
jalp21@icloud.com <jalp21@icloud.com>
2022-11-02 20:53:09 -0700

    Got the Javascript going

--- main.js
+++ main.js
@@ -1,40 +1,29 @@
-// Main varaibles
-
-    // Cstart button
-
-    // create function to register players choice
-// document.getElementById('buttonBullet').value = "You Chose";
-
-let bulletChosen = document.getElementById('buttonBullet');
-bulletChosen.addEventListener('click', playerChoice);
+//Player Chooses number 
 
 function playerChoice() {
-    console.log('hello');
+    let playerSelected = document.getElementById("testing123").value;
+ }
+//AI Chooses number
+let aiChoice = Math.floor(Math.random() * 6) + 1;
+
+//arrays
+
+let playerArray = [1, 2, 3, 4, 5, 6]
+ let aiArray = [1, 2, 3, 4, 5, 6]
+
+//array filter
+
+let aiShoot = playerArray.filter(afterAiShot)
+function afterAiShot(value) {
+    return value % 2 === 0;
 }
 
+//Player chooses another
 
-// Player shooting
-// function playersTurn () {
-//     if (playerChose(val) === aiChoice()) {
-//     console.log("you Win")
-//     } else {
+let playerShoot = aiArray.filter(playerAfterShot)
+function playerAfterShot(value) {
+    return
+}
 
-//     }
-// }
-    // create function to register players choice of attack 
-    // create function to grey out number once chosen if wrong
-    // Make color red if it is a hit
+// ai n player aray
 
-// Ai shooting
-
-    // create function to make ai choose random number
-    
-    // create function to grey out number once chosen if wrong
-    // Make color red if it is a hit
-   
-   
-// what happens when numbers are chosen
-
-    // create function to decide if player chose the bullet
-    // If the bulllet was chosen say "You Die"
-    // If the bullter was not chosen say 'You Live'
