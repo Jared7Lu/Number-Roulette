5788b976945d8350a5711ad5a96ceddb8ea56a57
jalp21@icloud.com <jalp21@icloud.com>
2022-11-06 20:47:31 -0800

    got some images going to represent player turn

--- READ.md
+++ READ.md
@@ -1,2 +1,18 @@
-A game of PG Russian Roulette with Numbers
+A game of PG Russian Roulette with Numbers.
+
+Inspiration
+    The inspiration for this game was Russian Roulette. My little brother and I, got bored one day and where trying to make a game and we ended playing Russian Roulette with numbers. It was a game that was reliant on memory and trust. 
+
+How To Play?
+    The first thing us for both players to chose the number in which is essentially where the bullet is. 
+
+    The first player then choses a number on the player's 2 side, and if it is the number that player 2 chose in the begining, then player 1 loses, if not then it's player 2 turn, untill either player loses or a draw is reached.
+
+Technologies used!
+    -Vs code
+    -Google images
+    -Git Hub
+
+Bugs?!?
+    One nnfinished functionality is images appearing in the center of the screen to show whos turn it is and wether they win or lose.
 
--- main.js
+++ main.js
@@ -2,41 +2,80 @@ function player1Choosing() {
   return Number(window.prompt("Type a number between 1 and 6", ""))
 }
 function player2Choosing() {
-    return Number(window.prompt("Type a number between 1 and 6", ""))
+    return Number(window.prompt("Type a number between 1 and 6", ""));
 }
 let player1Choice = 0
 let button1 = document.getElementById("chooseButton1")
 button1.addEventListener('click', () => {
    player1Choice = player1Choosing()
+   button1.disabled= true;
+   button1.style.visibility = "hidden";
 })
-
 let player2Choice = 0
 let button2 = document.getElementById("chooseButton2")
 button2.addEventListener("click", () => {
     player2Choice = player2Choosing()
+    button2.style.visibility = "hidden";
 })
 
-function player1Shot(e) {
-    var optionShot = document.getElementById('player2ShootingList').value
-    console.log(optionShot)
-    if (optionShot == player2Choice) {
-        prompt("Player 1 Died")
-    } else {
-        player2Shot();
-        alert("Player 1 live")
-        
-    } 
-    }
-function player2Shot(e) {
-    var optionShot2 = document.getElementById('player2ShootingList').value
-    console.log(optionShot2)
-    if (optionShot2 == player1Choice) {
-        alert("Player 2 Died")
-    } else {
-        alert ("Player 2 Lives")
-        player1Shot();
-    }
+//Shooting, Hopefully
+let buttons = document.querySelectorAll('#button');
+function showNumber(button) {
+  if (button.value == player2Choice) {
+     button.style.backgroundColor = "#FF0000"
+     playerdied()
+     resetButtonShow()
+  } else {
+    player2Turn()
+    button.style.backgroundColor = "#008000"
+    button.disabled = true;
+    
+  }
 }
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      showNumber(button)
+    });
+  }); 
+
+let buttons2 = document.querySelectorAll('#button2');
+function showNumber2(button) {
+  if (button.value == player1Choice) {
+     button.style.backgroundColor = "#FF0000"
+     playerdied()
+     resetButtonShow()
+  } else {
+    player1Turn()
+    button.style.backgroundColor = "#008000"
+    button.disabled = true;
+  }
+}
+buttons2.forEach(button => {
+
+    button.addEventListener("click", () => {
+      showNumber2(button)
+    });
+  }); 
+
+function player1Turn() {
+        var image = document.getElementById("imageDictator");
+        image.src="https://38.media.tumblr.com/d902edf4453bedd91b13d57a44b155bd/tumblr_n54rlu9jZV1t427mro2_400.gif";
+
+  }
+function player2Turn() {
+    var image = document.getElementById("imageDictator");
+    image.src = "https://media.giphy.com/media/iPuggAost1xde/giphy.gif";
+
+  }
+
+function playerdied() {
+    var image = document.getElementById("imageDictator");
+    image.src = "https://pngimg.com/uploads/game_over/game_over_PNG22.png";
+}
+function resetButtonShow() {
+    document.getElementById("resetButton").style.display = "block";
+}
+
 
 
 
