44a9323a1df730a80707e61734d1b1667a43ef23
jalp21@icloud.com <jalp21@icloud.com>
2022-11-06 21:15:58 -0800

    Getting the the play again button working

--- main.js
+++ main.js
@@ -8,28 +8,26 @@ let player1Choice = 0
 let button1 = document.getElementById("chooseButton1")
 button1.addEventListener('click', () => {
    player1Choice = player1Choosing()
-   button1.disabled= true;
+   button1.disabled = true;
    button1.style.visibility = "hidden";
 })
 let player2Choice = 0
 let button2 = document.getElementById("chooseButton2")
 button2.addEventListener("click", () => {
     player2Choice = player2Choosing()
+    button2.disabled = true;
     button2.style.visibility = "hidden";
 })
-
-//Shooting, Hopefully
 let buttons = document.querySelectorAll('#button');
 function showNumber(button) {
   if (button.value == player2Choice) {
-     button.style.backgroundColor = "#FF0000"
-     playerdied()
-     resetButtonShow()
+     button.style.backgroundColor = "#FF0000";
+     player1Died();
+     resetButtonShow();
   } else {
     player2Turn()
     button.style.backgroundColor = "#008000"
-    button.disabled = true;
-    
+    button.disabled = true;   
   }
 }
 buttons.forEach(button => {
@@ -42,7 +40,7 @@ let buttons2 = document.querySelectorAll('#button2');
 function showNumber2(button) {
   if (button.value == player1Choice) {
      button.style.backgroundColor = "#FF0000"
-     playerdied()
+     player2Died()
      resetButtonShow()
   } else {
     player1Turn()
@@ -51,26 +49,29 @@ function showNumber2(button) {
   }
 }
 buttons2.forEach(button => {
-
     button.addEventListener("click", () => {
       showNumber2(button)
     });
   }); 
-
 function player1Turn() {
         var image = document.getElementById("imageDictator");
         image.src="https://38.media.tumblr.com/d902edf4453bedd91b13d57a44b155bd/tumblr_n54rlu9jZV1t427mro2_400.gif";
-
   }
 function player2Turn() {
     var image = document.getElementById("imageDictator");
     image.src = "https://media.giphy.com/media/iPuggAost1xde/giphy.gif";
-
   }
-
-function playerdied() {
+function player1Died() {
     var image = document.getElementById("imageDictator");
-    image.src = "https://pngimg.com/uploads/game_over/game_over_PNG22.png";
+    image.src = "https://media.giphy.com/media/iPuggAost1xde/giphy.gif";
+    image.style.height = '450px';
+    image.style.width = '300px';
+}
+function player2Died() {
+    var image = document.getElementById("imageDictator");
+    image.src = "https://pngimg.com/uploads/game_over/game_over_PNG22.png" + img + "http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/d1e16859c09183b.png";
+    image.style.height = '450px';
+    image.style.width = '300px';
 }
 function resetButtonShow() {
     document.getElementById("resetButton").style.display = "block";
